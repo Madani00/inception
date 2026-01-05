adcb84acb71cbd4100efc8d0896bd824739f6f52
Madani00 <mamchart@gmail.com>
2026-01-05 20:23:17 +0100

    working on mariadb dockerfile along with the script

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+service mysql start
+
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MADANI_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MADANI_USER}\`@'localhost' IDENTIFIED BY '${MADANI_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MADANI_DATABASE}\`.* TO \`${MADANI_USER}\`@'%' IDENTIFIED BY '${MADANI_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MADANI_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe
