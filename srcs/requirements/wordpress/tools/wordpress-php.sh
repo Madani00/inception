6c50e2f3c536740220a157559db7bdce43f5b86d
Madani00 <mamchart@gmail.com>
2026-01-14 18:51:21 +0100

    trying to fix some issues in the script of wordpress and see if i can launch a home page

--- srcs/requirements/wordpress/tools/wordpress-php.sh
+++ srcs/requirements/wordpress/tools/wordpress-php.sh
@@ -14,5 +14,16 @@ chmod +x wp-cli.phar
 # php /var/www/html/wp-cli.phar  core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
 
+MADANI_USER=madanidb
+MADANI_PASSWORD=madani_password
+MADANI_DATABASE=madani_db
+
+wp-cli.phar config create	--allow-root \
+							--dbname=$MADANI_DATABASE \
+							--dbuser=$MADANI_USER \
+							--dbpass=$MADANI_PASSWORD \
+							--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+
 # finally launch it
 /usr/sbin/php-fpm7.3 -F
